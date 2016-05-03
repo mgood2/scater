21aa9587f1cba6745405ca843c49249ce299e6db
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-03 21:26:31 +0000

    Creating branch for BioC 3.3 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/scater@117080 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/test-plotting.R
@@ -0,0 +1,68 @@
+## tests for plotting functions
+
+context("test plotPCA, plotTSNE and plotDiffusionMap")
+
+test_that("we can produce PCA plots with different expression values", {
+    data("sc_example_counts")
+    data("sc_example_cell_info")
+    pd <- new("AnnotatedDataFrame", data = sc_example_cell_info)
+    example_sceset <- newSCESet(countData = sc_example_counts, phenoData = pd)
+    example_sceset <- calculateQCMetrics(example_sceset)
+
+    expect_that(plotPCA(example_sceset), is_a("ggplot"))
+})
+
+test_that("we can produce t-SNE plots with different expression values", {
+    data("sc_example_counts")
+    data("sc_example_cell_info")
+    pd <- new("AnnotatedDataFrame", data = sc_example_cell_info)
+    example_sceset <- newSCESet(countData = sc_example_counts, phenoData = pd)
+    example_sceset <- calculateQCMetrics(example_sceset)
+
+    expect_that(plotTSNE(example_sceset), is_a("ggplot"))
+})
+
+test_that("we can produce Diffusion Map plots with different expression values",
+          {
+              data("sc_example_counts")
+              data("sc_example_cell_info")
+              pd <- new("AnnotatedDataFrame", data = sc_example_cell_info)
+              example_sceset <- newSCESet(countData = sc_example_counts,
+                                          phenoData = pd)
+              example_sceset <- calculateQCMetrics(example_sceset)
+
+              expect_that(plotDiffusionMap(example_sceset), is_a("ggplot"))
+          })
+
+test_that("we can produce MDS plots with different expression values",
+          {
+              data("sc_example_counts")
+              data("sc_example_cell_info")
+              pd <- new("AnnotatedDataFrame", data = sc_example_cell_info)
+              example_sceset <- newSCESet(countData = sc_example_counts,
+                                          phenoData = pd)
+              example_sceset <- calculateQCMetrics(example_sceset)
+              cellDist(example_sceset) <- as.matrix(
+                  dist(t(exprs(example_sceset))))
+
+              expect_that(plotMDS(example_sceset), is_a("ggplot"))
+              expect_that(plotMDS(example_sceset,
+                                          colour_by = "Cell_Cycle",
+                                          shape_by = "Treatment",
+                                          size_by = "Mutation_Status"),
+                            is_a("ggplot"))
+          })
+
+context("test plotExpression")
+
+test_that("we can produce expression plots with different expression values", {
+    data("sc_example_counts")
+    data("sc_example_cell_info")
+    pd <- new("AnnotatedDataFrame", data = sc_example_cell_info)
+    example_sceset <- newSCESet(countData = sc_example_counts, phenoData = pd)
+    example_sceset <- calculateQCMetrics(example_sceset)
+
+    expect_that(plotExpression(example_sceset, 1:4, "Cell_Cycle"), is_a("ggplot"))
+    expect_that(plotExpression(example_sceset, 1:4, "Gene_0004"), is_a("ggplot"))
+})
+
---
+++ tests/testthat/test-tximport-wrapper.R
@@ -0,0 +1,11 @@
+# Tests for tximport wrapper
+
+context("test imporper input usage")
+
+test_that("failure is as expected for wrong input", {
+    directories <- file.path("foo", "bar", 1:10)
+    files <- file.path(directories, "abundance.tsv")
+    expect_that(readTxResults(samples = 1:10, files = files,
+                              type = "kallisto"),
+                    throws_error("Some of the files provided do not exist!"))
+})
